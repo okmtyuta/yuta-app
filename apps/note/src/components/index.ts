94d036fc4ff2b3db54b8476190b2b5a2a7b53a43
okmtyuta <okmtyuta@gmail.com>
2024-05-21 13:16:57 +0900

    post

--- apps/note/src/components/index.ts
+++ apps/note/src/components/index.ts
@@ -1,6 +1,5 @@
 import Header from './header/header.astro'
 import Footer from './footer/footer.astro'
 import Frame from './frame/frame.astro'
-import Postcard from './postcard/postcard.astro'
 
-export { Header, Footer, Frame, Postcard }
+export { Header, Footer, Frame,  }
