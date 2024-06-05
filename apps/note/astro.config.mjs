124deee7ab984501e995ea4df1152866519bd2fa
okmtyuta <okmtyuta@gmail.com>
2024-06-05 11:59:00 +0900

    書きかけ: Brief Summary of Confidence sets for persistence diagrams

--- apps/note/astro.config.mjs
+++ apps/note/astro.config.mjs
@@ -3,23 +3,20 @@ import remarkMath from 'remark-math'
 import remarkDirective from 'remark-directive'
 import rehypeKatex from 'rehype-katex'
 import remarkLinkCard from 'remark-link-card'
-import { amsthm } from './thmenv'
+import { amsthm } from './src/markdown/amsthm'
+
+const katexOption = {
+  macros: {
+    '\\kakko': '\\left(#1\\right)'
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   markdown: {
     remarkPlugins: [remarkMath, remarkDirective, remarkLinkCard, amsthm],
-    rehypePlugins: [
-      [
-        rehypeKatex,
-        {
-          macros: {
-            '\\kakko': '\\left(#1\\right)'
-          }
-        }
-      ]
-    ]
+    rehypePlugins: [[rehypeKatex, katexOption]]
   },
   integrations: [],
   image: {
